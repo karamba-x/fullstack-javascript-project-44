a1781f4f73f67f53ae3bc63d4be1b3df074d9f12
karamba <kadykov.x@gmail.com>
2023-12-02 02:02:19 +0900

    debug

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,8 @@ import readlineSync from 'readline-sync';
 
 const whoYou = () => {
     const name = readlineSync.question('May I have your name? ');
-    console.log('Hello', name);
+    console.log('Hello,', name);
     return name;
 }
-
-
 
 export default whoYou;
