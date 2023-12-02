02470cb458a616d5a7d6664f0acbbcbcf0c5599c
karamba <kadykov.x@gmail.com>
2023-12-02 23:48:32 +0900

    fix

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,12 +1,13 @@
-const isPrime = (num) => {
-    for (let i = 2; i < num; i++) {
-      if (num % i === 0) return 'no';
-    }
-    if (num !== 0) {
-        return 'yes';
-    } else {
+const isPrime = (n) => {
+    if (n <= 1)
         return 'no';
-    }
+
+    // Check from 2 to n-1 
+    for (let i = 2; i < n; i++)
+        if (n % i == 0)
+            return 'no';
+
+    return 'yes';
 }
 
 export default isPrime;
